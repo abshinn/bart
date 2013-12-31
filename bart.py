8d112d880c57cbe51f03adab9eafdda450b77f20
Adam Shinn <adam.b.shinn@gmail.com>
2013-12-30 17:14:47 -0800

    restructured class hierarchy to match the different BART APIs (i.e. bart.bsa.bsa() calls the BART Service Advisory API's bsa command)

--- bart.py
+++ bart.py
@@ -4,7 +4,6 @@ import os, sys, urllib.request, urllib.parse
 import xml.etree.ElementTree as ET
 import pdb
 
-
 envkey = os.environ.get("BART_API_KEY")
 if envkey:
     key = envkey
@@ -12,21 +11,22 @@ else:
     print("BART_API_KEY environment variable does not exist")
     sys.exit()
 
-def APIquery(queryType = "", **parameters):
+def APIquery(API = "", **parameters):
     """bart.APIquery(**parameters) retrieves the xmltree root object from http://api.bart.gov"""
-    if not queryType:
+    if not API:
         try:
-            queryType = parameters["cmd"]
+            API = parameters["cmd"]
         except KeyError:
             print("cmd is a required keyword")
             sys.exit()
     parameters["key"] = key
     queryStr = urllib.parse.urlencode(parameters)
-    url = "http://api.bart.gov/api/{}.aspx?{}".format(queryType, queryStr)
+    url = "http://api.bart.gov/api/{}.aspx?{}".format(API, queryStr)
     #print("Querying BART with: {}".format(url))
     with urllib.request.urlopen(url) as BART:
         tree = ET.parse(BART)
-    return tree.getroot()
+    root = tree.getroot()
+    return root
 
 class BSAstruct:
     """bart.BSAstruct(): handles the xmltree root specific to the BART API bsa command"""
@@ -49,14 +49,6 @@ class BSAstruct:
                          "Description: [{}] {}\n".format(bsa["station"], bsa["type"], bsa["descrip"])
         return strResult
 
-class bsa(object):
-    """bart.bsa(): BART Service Advisory"""
-    def __init__(self, **parameters):
-        self.root = APIquery(cmd = "bsa", **parameters)
-        self.result = BSAstruct(self.root)
-    def __str__(self):
-        return self.result.__str__()
-
 class COUNTstruct:
     """bart.COUNTstruct(): handles the xmltree root specific to the BART API count command"""
     def __init__(self, xmlroot):
@@ -64,15 +56,7 @@ class COUNTstruct:
         self.time = xmlroot.find("time").text
         self.traincount = xmlroot.find("traincount").text
     def __str__(self):
-        return "BART train count {self.date}; {self.time}\n{self.traincount} trains".format(self=self)
-
-class count(object):
-    """bart.count(): BART Train Count"""
-    def __init__(self, **parameters):
-        self.root = APIquery(queryType = "bsa", cmd = "count", **parameters)
-        self.result = COUNTstruct(self.root)
-    def __str__(self):
-        return self.result.__str__()
+        return "BART train count {self.date}; {self.time}\n{self.traincount} trains\n".format(self=self)
 
 class ELEVstruct:
     """bart.ELEVstruct(): handles the xmltree root specific to the BART API elev command"""
@@ -92,15 +76,44 @@ class ELEVstruct:
                          "Description: [{}] {}\n".format(bsa["station"], bsa["type"], bsa["descrip"])
         return strResult
 
-class elev(object):
-    """bart.elev(): BART Elevator Advisory"""
-    def __init__(self, **parameters):
-        self.root = APIquery(queryType = "bsa", cmd = "elev", **parameters)
-        self.result = ELEVstruct(self.root)
-    def __str__(self):
-        return self.result.__str__()
+
+class bsa:
+    """bart.bsa: BART Service Advisory API"""
+
+    class bsa(object):
+        """bart.bsa.bsa()"""
+        def __init__(self, **parameters):
+            self.root = APIquery(API = "bsa", cmd = "bsa", **parameters)
+            #self.string = ET.tostring(self.root, encoding = "unicode") # string result
+            self.result = BSAstruct(self.root)
+        def __str__(self):
+            return self.result.__str__()
+
+    class count(object):
+        """bart.bsa.count(): BART Train Count"""
+        def __init__(self, **parameters):
+            self.root = APIquery(API = "bsa", cmd = "count", **parameters)
+            self.result = COUNTstruct(self.root)
+        def __str__(self):
+            return self.result.__str__()
+
+    class elev(object):
+        """bart.bsa.elev(): BART Elevator Advisory"""
+        def __init__(self, **parameters):
+            self.root = APIquery(API = "bsa", cmd = "elev", **parameters)
+            self.result = ELEVstruct(self.root)
+        def __str__(self):
+            return self.result.__str__()
+
+class STNSstruct:
+    """bart.STNSstruct(): handles the xmltree root specific to the BART API stns command"""
+    def __init__(self, xmlroot):
+        self.stations = xmlroot.find("station")
+        for station in stations.findall("station"):
+            pass
+
 
 if __name__ == "__main__":
-    print(bsa())
-    print(count())
-    print(elev())
+    print(bsa.bsa())
+    print(bsa.count())
+    print(bsa.elev())
