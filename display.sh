a1525e8fb87274ef00b0c742f69f75df619a636e
Adam Shinn <adam.b.shinn@gmail.com>
2014-06-30 13:32:08 -0700

    add bash loop script

---
+++ display.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# if [ -z "$1" == "-t"]
+#     then  sleep=$2
+#         while :; do clear; $3 $4 $5 ; sleep $sleep; done
+#     else
+while :; do clear; $1 $2 $3 ; sleep 59; done
+# fi
