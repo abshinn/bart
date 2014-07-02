4f6652df142d89d1804f7fb562624237f2a4e00b
Adam Shinn <adam.b.shinn@gmail.com>
2014-07-02 15:51:41 -0700

    delete display.sh

--- display.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# if [ -z "$1" == "-t"]
-#     then  sleep=$2
-#         while :; do clear; $3 $4 $5 ; sleep $sleep; done
-#     else
-while :; do clear; $1 $2 $3 ; sleep 59; done
-# fi
