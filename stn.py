50740a1f26e106bf628b1427da7c76ebc0bd48a4
Adam Shinn <adam.b.shinn@gmail.com>
2014-07-01 18:50:13 -0700

    add argparser for command-line input; add tracker

--- stn.py
+++ stn.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python2.7 -tt
 
 import urllib
-# import requests # <-- see if faster than urllib
 import os, sys
-# import os, sys, urllib.request, urllib.parse
+import argparse
 import xml.etree.ElementTree as ET
+# import pdb
 
-def stn(orig = "plza", direct="s"):
+
+def stn(origin="plza", direction="s"):
     # obtain system variable BART_API_KEY
     key = os.environ.get("BART_API_KEY")
 
     # build url
-    url = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig={}&dir={}&key={}'.format(orig, direct, key)
+    url = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig={}&dir={}&key={}'.format(origin, direction, key)
 
-    with urllib.urlopen(url) as webpage:
-        tree = ET.parse(webpage)
-        root = tree.getroot()
-
-#     try:
-#         webpage = urllib.urlopen(url)
-#     except IOError: 
-#         sys.stderr.write("Couldn't connect to {}\n".format(url))
-#         return 
-#     except ValueError as ValueMsg:
-#         sys.stderr.write("Value Error: {}\n".format(ValueMsg))
-#         return
+    try:
+        webpage = urllib.urlopen(url)
+    except IOError as msg:
+        sys.stderr.write("IOError: {}\nCould not connect to API:\n\t{}".format(msg, url))
+        return 
+    tree = ET.parse(webpage)
+    webpage.close()
+    root = tree.getroot()
 
     date = root.find('date').text
     time = root.find('time').text
     time = time[:-3]
     station = root.find('station/name').text
-    print(station, date, time)
+    print "{} at {}".format(station, time)
 
     for etd in root.findall('station/etd'):
         dest = etd.find('destination').text
-#         print(dest)
         for estimate in etd.findall('estimate'):
             minutes = estimate.find('minutes').text
             platform = estimate.find('platform').text
             direction = estimate.find('direction').text
-            length = estimate.find("length").text
+            length = estimate.find('length').text
             if minutes.lower() == 'leaving':
                 minutes = 'now ' + minutes
             else:
@@ -48,13 +44,27 @@ def stn(orig = "plza", direct="s"):
                 if intmins == 1:
                     minutes = minutes[:-1]
 #             print("{:>2} car {} train in {} on platform {},".format(length, dest, minutes, platform))
-            print("{:>2} car {} train {},".format(length, dest, minutes))
+            print("{:>2} car {:<20} train {},".format(length, dest, minutes))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        stn(orig = sys.argv[1])
-    elif len(sys.argv) == 3:
-        stn(orig = sys.argv[1], direct = sys.argv[2])
+    parser = argparse.ArgumentParser() 
+    parser.add_argument(  "station", help="display schedule for given station")
+    parser.add_argument("direction", help="display routes for a given direction")
+    parser.add_argument("-t", "--tracker", help="tracker: repeat command every minute until keyboard interrupt", action="store_true")
+    args = parser.parse_args()
+    if args.tracker:
+        try:
+            while 1:
+                os.system("clear")
+                print "{} {}".format(args.station, args.direction)
+                stn(origin = args.station, direction = args.direction)
+                exitstatus = os.system("sleep 59")
+                if exitstatus:
+                    break
+        except msg:
+            print msg
+            print "catch keyboard error?"
+            pass
     else:
-        stn(orig = "plza", direct = "s")
+        stn(origin = args.station, direction = args.direction)
